46512f21415e8e9d8297fc5bd9647cf62c89ce00
samrodd <loughlinrodd@gmail.com>
2023-07-23 16:31:43 -0400

    Update generate-naive-bayes-dataset.py

--- naive-bayes-classifier/generate-naive-bayes-dataset.py
+++ naive-bayes-classifier/generate-naive-bayes-dataset.py
@@ -26,7 +26,7 @@ dataset = generate_synthetic_dataset(num_samples=1000)
 print(dataset.head())
 
 # Export the dataset to a CSV file
-file_path = 'filepatch'  # Replace with the actual file path and name
+file_path = 'filepath'  # Replace with the actual file path and name
 export_to_csv(dataset, file_path)
 
 print("Dataset exported to CSV successfully.")
